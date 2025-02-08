c791f061e7bb1a2c484a0237a91b39cf69985760
hzlucasf <hzlucasf@gmail.com>
2025-02-08 13:27:09 -0300

    update waifu service

--- src/main/java/com/hzlucasf/waifu/service/WaifuService.java
+++ src/main/java/com/hzlucasf/waifu/service/WaifuService.java
@@ -1,7 +1,13 @@
 package com.hzlucasf.waifu.service;
 
+import com.hzlucasf.waifu.repository.WaifuRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WaifuService {
+    private final WaifuRepository waifuRepository;
+
+    public WaifuService(WaifuRepository waifuRepository) {
+        this.waifuRepository = waifuRepository;
+    }
 }
