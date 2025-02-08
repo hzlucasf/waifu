64ccb8fb01fea06e737361febdec99a50228bb4f
hzlucasf <hzlucasf@gmail.com>
2025-02-08 13:30:09 -0300

    add method to find all waifus

--- src/main/java/com/hzlucasf/waifu/service/WaifuService.java
+++ src/main/java/com/hzlucasf/waifu/service/WaifuService.java
@@ -1,7 +1,10 @@
 package com.hzlucasf.waifu.service;
 
+import com.hzlucasf.waifu.model.Waifu;
 import com.hzlucasf.waifu.repository.WaifuRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class WaifuService {
@@ -9,5 +12,9 @@ public class WaifuService {
 
     public WaifuService(WaifuRepository waifuRepository) {
         this.waifuRepository = waifuRepository;
+    }
+
+    public List<Waifu> findAll() {
+        return waifuRepository.findAll();
     }
 }
