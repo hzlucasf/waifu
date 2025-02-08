79392771617a7aa09769423f939cd15cf8a3f1e7
hzlucasf <hzlucasf@gmail.com>
2025-02-08 13:42:03 -0300

    update anime service

--- src/main/java/com/hzlucasf/waifu/service/AnimeService.java
+++ src/main/java/com/hzlucasf/waifu/service/AnimeService.java
@@ -1,7 +1,13 @@
 package com.hzlucasf.waifu.service;
 
+import com.hzlucasf.waifu.repository.AnimeRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AnimeService {
+    private final AnimeRepository animeRepository;
+
+    public AnimeService(AnimeRepository animeRepository) {
+        this.animeRepository = animeRepository;
+    }
 }
