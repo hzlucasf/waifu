73fffa63166cc96be5d89fcdc1bcdfb42e013822
hzlucasf <hzlucasf@gmail.com>
2025-02-08 13:33:54 -0300

    add method to find waifu by id

--- src/main/java/com/hzlucasf/waifu/service/WaifuService.java
+++ src/main/java/com/hzlucasf/waifu/service/WaifuService.java
@@ -1,10 +1,12 @@
 package com.hzlucasf.waifu.service;
 
+import com.hzlucasf.waifu.exception.WaifuNotFoundException;
 import com.hzlucasf.waifu.model.Waifu;
 import com.hzlucasf.waifu.repository.WaifuRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class WaifuService {
@@ -16,5 +18,15 @@ public class WaifuService {
 
     public List<Waifu> findAll() {
         return waifuRepository.findAll();
+    }
+
+    public Waifu findById(UUID id) {
+        var optionalWaifu = waifuRepository.findById(id);
+
+        if (optionalWaifu.isEmpty()) {
+            throw new WaifuNotFoundException("waifu not found");
+        }
+
+        return optionalWaifu.get();
     }
 }
