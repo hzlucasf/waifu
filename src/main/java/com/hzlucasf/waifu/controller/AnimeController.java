2b3e5f6742eb67c0bd3588b93b35b2838ac17120
hzlucasf <hzlucasf@gmail.com>
2025-02-08 13:42:50 -0300

    update anime controller

--- src/main/java/com/hzlucasf/waifu/controller/AnimeController.java
+++ src/main/java/com/hzlucasf/waifu/controller/AnimeController.java
@@ -1,9 +1,15 @@
 package com.hzlucasf.waifu.controller;
 
+import com.hzlucasf.waifu.service.AnimeService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/animes")
 public class AnimeController {
+    private final AnimeService animeService;
+
+    public AnimeController(AnimeService animeService) {
+        this.animeService = animeService;
+    }
 }
