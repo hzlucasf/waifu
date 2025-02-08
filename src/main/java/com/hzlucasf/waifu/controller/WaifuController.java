8b31c7c44c77966ba2c87553c5e513f6cd6fc589
hzlucasf <hzlucasf@gmail.com>
2025-02-08 13:28:04 -0300

    update waifu controller

--- src/main/java/com/hzlucasf/waifu/controller/WaifuController.java
+++ src/main/java/com/hzlucasf/waifu/controller/WaifuController.java
@@ -1,9 +1,15 @@
 package com.hzlucasf.waifu.controller;
 
+import com.hzlucasf.waifu.service.WaifuService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/waifus")
 public class WaifuController {
+    private final WaifuService waifuService;
+
+    public WaifuController(WaifuService waifuService) {
+        this.waifuService = waifuService;
+    }
 }
