fd309342209b45b79ae68e42faa9ee8c1305837e
hzlucasf <hzlucasf@gmail.com>
2025-02-09 19:25:07 -0300

    update anime repository

--- src/main/java/com/hzlucasf/waifu/repository/AnimeRepository.java
+++ src/main/java/com/hzlucasf/waifu/repository/AnimeRepository.java
@@ -1,11 +1,17 @@
 package com.hzlucasf.waifu.repository;
 
 import com.hzlucasf.waifu.model.Anime;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface AnimeRepository extends JpaRepository<Anime, UUID> {
+    @Override
+    @NonNull
+    Page<Anime> findAll(@NonNull Pageable pageable);
 }
