321e0cbd1d4e546779c7879a6194115fffc885d4
hzlucasf <hzlucasf@gmail.com>
2025-02-09 19:24:41 -0300

    update waifu repository

--- src/main/java/com/hzlucasf/waifu/repository/WaifuRepository.java
+++ src/main/java/com/hzlucasf/waifu/repository/WaifuRepository.java
@@ -1,11 +1,17 @@
 package com.hzlucasf.waifu.repository;
 
 import com.hzlucasf.waifu.model.Waifu;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface WaifuRepository extends JpaRepository<Waifu, UUID> {
+    @Override
+    @NonNull
+    Page<Waifu> findAll(@NonNull Pageable pageable);
 }
