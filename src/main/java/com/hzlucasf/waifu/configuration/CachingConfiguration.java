77322e163f95fb56b568fedfbe872f6095b8f068
hzlucasf <hzlucasf@gmail.com>
2025-02-09 20:24:00 -0300

    add cache manager bean

--- src/main/java/com/hzlucasf/waifu/configuration/CachingConfiguration.java
+++ src/main/java/com/hzlucasf/waifu/configuration/CachingConfiguration.java
@@ -1,9 +1,36 @@
 package com.hzlucasf.waifu.configuration;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
 
 @Configuration
 @EnableCaching
 public class CachingConfiguration {
+    @Bean
+    public CacheManager getCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        var redisCacheConfiguration = RedisCacheConfiguration
+                .defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(30))
+                .disableCachingNullValues()
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new GenericJackson2JsonRedisSerializer()
+                        )
+                );
+
+        return RedisCacheManager
+                .builder(RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory))
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
 }
